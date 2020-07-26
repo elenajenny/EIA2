d345a71e00bb7d2d23297eb9a1e1bcd621063735
elenajenny <elena@chiang-mayer.de>
2020-07-26 12:22:06 +0200

    another commit

--- Abschlussarbeit/Code/Main.js
+++ Abschlussarbeit/Code/Main.js
@@ -4,7 +4,20 @@ var MagicCanvas;
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
         console.log("handleLoad");
+        let rule = document.querySelector("#rules");
+        rule.addEventListener("click", rulesVisibility);
+        handleCanvasSize();
         generateSymbols(_event);
+    }
+    function rulesVisibility() {
+        console.log("show rules");
+        let rulesdiv = document.querySelector("#overlay");
+        if (rulesdiv.style.display == "none") {
+            rulesdiv.style.display = "block";
+        }
+        else {
+            rulesdiv.style.display = "none";
+        }
     }
     function generateSymbols(_event) {
         console.log("generate Symbols");
--- Abschlussarbeit/Code/Main.ts
+++ Abschlussarbeit/Code/Main.ts
@@ -4,18 +4,31 @@ namespace MagicCanvas {
     function handleLoad(_event: Event): void {
         console.log("handleLoad");
 
+        let rule: HTMLElement = <HTMLElement>document.querySelector("#rules");
+        rule.addEventListener("click", rulesVisibility);
+
+        handleCanvasSize();
+
         generateSymbols(_event);
     }
+
+    function rulesVisibility(): void {
+        console.log("show rules");
+        let rulesdiv: HTMLElement = <HTMLElement>document.querySelector("#overlay");
+        
+        if (rulesdiv.style.display == "none") {
+        rulesdiv.style.display = "block";
+        } else {
+        rulesdiv.style.display = "none";
+        }
+    }
+
+
+    
 
     function generateSymbols (_event: Event): void {
         console.log("generate Symbols");
     }
-
-
-
-
-
-
-
+    
 
 }
